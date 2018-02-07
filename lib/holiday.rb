f0d59ecb6a96b7087b7cdc234a3fa80ddae243e0
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2018-02-07 03:13:09 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -88,7 +88,8 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |seasons, holidays|
+    puts '#{seasons}'
 end
 
 def all_holidays_with_bbq(holiday_hash)

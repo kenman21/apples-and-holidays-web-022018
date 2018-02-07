c7cfdf5a8b7593cbf33d57acfe61a32c90dd84e7
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2018-02-07 03:22:55 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -91,7 +91,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |seasons, holidays|
     puts "#{seasons.capitalize}:"
     holidays.each do |holiday, supplies|
-      puts "#{holiday.capitalize}: #{supplies.join(", ")}" 
+      puts "   #{holiday.capitalize}: #{supplies.join(", ")}" 
     end 
   end 
 end

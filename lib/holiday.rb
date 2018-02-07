20c3f699aad9d2711856ef51a6d475f885174662
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2018-02-07 03:47:11 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -100,6 +100,13 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  holiday_hash.each do |seasons, holidays|
+    holidays.each do |holiday, supplies|
+      if supplies == "BBQ"
+        bbqarray.push(holiday)
+      end 
+    end 
+  end 
 
 end
 
